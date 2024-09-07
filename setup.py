563a0e2abaf7707a8102dc4ee7452e5cb362b7da
SureshE14 <86488759+SureshE14@users.noreply.github.com>
2024-09-07 19:10:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = "neo-api-client"
-VERSION = "1.2.0"
+VERSION = "1.21.2"
 # To install the library, run the following
 #
 # python setup.py install
